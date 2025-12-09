ca7b21ef04ecf24d28aa8e4a622ce1ff3d290c0e
kpidemo <for_students@ukr.net>
2025-12-09 09:54:31 +0200

    Refactor calc.c with additional comments

--- calc.c
+++ calc.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 void main() {
+    // more changes
     printf("Calculator runs");
     printf("Second line");
     printf("Third line");
     printf("Changes for new release");
     // Added comment for code changes
 }
+
 
 
 
